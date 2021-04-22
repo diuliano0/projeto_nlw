075e0c92fa0bf25fca57f4a298fa8e06f7a48e56
diuliano de sousa oliveira <diuliano0@gmail.com>
2021-04-22 18:42:40 -0300

    feature/Aula 03

---
+++ src/controllers/UsersController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { UsersService } from "../services/UsersService";
+
+class UsersController {
+  async create(request: Request, response: Response): Promise<Response>{
+    const {email} = request.body;
+    const usersService = new UsersService();
+    try{
+      const users = usersService.create({email});
+      return response.json(users);
+    }catch(err) {
+      return response.status(400).json({
+        message: err.message,
+      });
+    }
+  }
+}
+export { UsersController };
