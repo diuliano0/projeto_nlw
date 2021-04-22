26a24080d9ece974ddd1a9e9153bd05013291c4c
diuliano de sousa oliveira <diuliano0@gmail.com>
2021-04-22 18:46:24 -0300

    Revert "Revert "feature/Aula 03""
    This reverts commit 43560163e46ead6c05c4f4e00469bd331c674ac7.

---
+++ src/services/UsersService.ts
@@ -0,0 +1,29 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository";
+
+interface IUserCreate{
+  email: string;
+}
+
+class UsersService{
+  private usersRepository: Repository<User>;
+
+  constructor(){
+    this.usersRepository = getCustomRepository(UsersRepository);
+  }
+  async create({email}: IUserCreate) {
+    const userExists = await this.usersRepository.findOne({
+      email,
+    });
+    if(userExists){
+      return userExists;
+    }
+    const users = this.usersRepository.create({
+      email
+    });
+    await this.usersRepository.save(users);
+    return users;
+  }
+}
+export { UsersService };
